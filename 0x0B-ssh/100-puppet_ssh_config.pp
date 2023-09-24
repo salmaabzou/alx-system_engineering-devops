ea02064c6d1ea4cac5b0fe3fb067b84a98cf6722
salmaabzou <salmaabzou@gmail.com>
2023-09-24 23:22:09 +0100

    puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,9 @@
+# Set up server configuration with puppet
+file_line{'Set alias name':
+  path => '/etc/ssh/ssh_config',
+  line => 'Host 54.226.41.95
+    HostName 54.226.41.95
+    User ubuntu
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no',
+}
